a364b0a614cb57eb4131353bae1be951abc10709
Vasantha-idexcel <vasantha.r@idexcel.com>
2023-04-23 21:39:27 +0530

    Added notes route

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
 import { topicsRouter } from "./routers/topics";
+import { notesRouter } from "./routers/notes";
 
 /**
  * This is the primary router for your server.
@@ -9,7 +10,8 @@ import { topicsRouter } from "./routers/topics";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
-  topics: topicsRouter
+  topics: topicsRouter,
+  notes: notesRouter,
 });
 
 // export type definition of API
