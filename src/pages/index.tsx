65c004725cbc4035f81bd7f6e85099888acf7764
Vasantha-idexcel <vasantha.r@idexcel.com>
2023-04-23 21:31:43 +0530

    Topics list and create

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,73 @@
 import { type NextPage } from "next";
-
-import { api } from "~/utils/api";
 import Header from "./components/Header";
+import React, { useState } from "react";
+import { useSession } from "next-auth/react";
+import { api } from "~/utils/api";
+import { Topic } from "@prisma/client";
+
+const Content: React.FC = () => {
+  const { data: sessionData } = useSession();
+  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
+
+  const { data: topics, refetch: refetchTopics } = api.topics.getAll.useQuery(
+    undefined,
+    {
+      enabled: sessionData?.user !== undefined,
+      onSuccess: (data) => {
+        setSelectedTopic(selectedTopic ?? data[0] ?? null);
+      },
+    }
+  );
+
+  const createTopic = api.topics.create.useMutation({
+    onSuccess: () => {
+      refetchTopics();
+    },
+  });
+
+  return (
+    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+      <div className="px-2">
+        <ul className="menu rounded-box w-56 bg-base-100 p-2">
+          {topics?.map((topic) => (
+            <li key={topic.id}>
+              <a
+                href="#"
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  setSelectedTopic(topic);
+                }}
+              >
+                {topic.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          placeholder="New Topic"
+          className="input-bordered input input-sm w-full"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              createTopic.mutate({
+                title: e.currentTarget.value,
+              });
+              e.currentTarget.value = "";
+            }
+          }}
+        />
+      </div>
+      <div className="col-span-3"></div>
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
   return (
     <>
       <main>
         <Header />
+        <Content />
       </main>
     </>
   );
